27362fbb55c9842c64e320cf87b0684af240ad9d
Mohammad Aman <114508170+MohammadAman5577@users.noreply.github.com>
2024-10-30 23:42:08 +0530

    var in js

---
+++ Video 55/script.js
@@ -0,0 +1,12 @@
+var a= 1
+var b =4
+var c="Hello aman"
+console.log(a+b+c)
+console.log(typeof a, typeof b, typeof c)
+let Student ={
+    name: "Mohammad Aman", isHandsome: true,
+    salary:"50lpa", sibleings:"yes"
+}
+console.log(Student)
+Student.salary="90lpa";
+console.log(Student)
