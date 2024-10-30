9326a25bf87b4dd4498893e07dace3cefaea6739
Mohammad Aman <114508170+MohammadAman5577@users.noreply.github.com>
2024-10-30 23:41:41 +0530

    script

---
+++ Video 54/script.js
@@ -0,0 +1,16 @@
+alert("Hello Baccho");
+console.log("Hey i am a console")
+console.log("Hey i am a console")
+console.log("Hey i am a console")
+
+var a = prompt("Enter your number")
+console.log("your number is "+a)
+
+var isTrue=confirm("Your computer is gonna burn if u leave")
+if(isTrue){
+    console.log("Your computer is blasting")
+}
+else{
+    console.log("not blasitng")
+}
+document.body.style.backgroundColor="red"
