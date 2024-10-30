c88dce22f6a27b40ff54ef20df43cb7f4d62fc74
Mohammad Aman <114508170+MohammadAman5577@users.noreply.github.com>
2024-10-30 23:42:27 +0530

    loops in js

---
+++ Video 56/script.js
@@ -0,0 +1,12 @@
+console.log("Hello  aman bhai")
+
+let age = 2
+if (age>18){
+    console.log("You are elligible to vote ")
+
+}
+else{
+    console.log("You cannot vote")
+}
+let beta= 4
+console.log(beta**age)
