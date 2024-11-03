7ace7351d0e962ef78b99b204472e185d48ca1b9
Mohammad Aman <114508170+MohammadAman5577@users.noreply.github.com>
2024-11-04 00:29:29 +0530

    Create script.js
    function

---
+++ Video 58/script.js
@@ -0,0 +1,20 @@
+function Aman(name) {
+    console.log("hey! "+name+" You are awesome")    
+    console.log("hey! "+name+" You are awesome")    
+    console.log("hey! "+name+" You are awesome")    
+    console.log("hey! "+name+" You are awesome")    
+}
+Aman("anas")
+function sum (a,b){
+    console.log(a+b)
+    return a+b
+}
+result = sum(5,3)
+console.log("the sume is ",result)
+
+const func1 = (x)=>{
+    console.log("i am an arrow functoin"+x)
+}
+func1(314)
+func1(324)
+func1(342)
