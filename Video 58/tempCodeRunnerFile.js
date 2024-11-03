9e8fd0d09109d48e0ee623c9e0638b786bb81890
Mohammad Aman <114508170+MohammadAman5577@users.noreply.github.com>
2024-11-04 00:29:06 +0530

    Create tempCodeRunnerFile.js
    learnig  about consoles

---
+++ Video 58/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+function Aman(name) {
+    console.log("hey! "+name+" You are awesome")    
+    console.log("hey! "+name+" You are awesome")    
+    console.log("hey! "+name+" You are awesome")    
+    console.log("hey! "+name+" You are awesome")    
+}
+Aman("anas")
+Aman("Ashfa")
