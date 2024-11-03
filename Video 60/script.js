477d7178f3a37afc012de3e3fd51d2e3aac15b62
Mohammad Aman <114508170+MohammadAman5577@users.noreply.github.com>
2024-11-04 00:30:18 +0530

    Create script.js
    literals

---
+++ Video 60/script.js
@@ -0,0 +1,8 @@
+console.log("This is a Strings Tutorial")
+
+let name="Aman"
+console.log(name)
+let real_name="M Aman"
+let nick_name="Ammu"
+
+console.log(`My name is ${real_name} ,my parents call me ${nick_name}`)
