794ee20cbf676189736e2e54672662fbd9d25a01
Mohammad Aman <114508170+MohammadAman5577@users.noreply.github.com>
2025-01-29 22:06:23 +0530

    Create aboutpath.js

---
+++ Video 87/aboutpath.js
@@ -0,0 +1,9 @@
+import path from "path"
+
+
+let myPath = "C:\\Users\\moham\\OneDrive\\Desktop\\Sigma Web Development\\Sigma-Web_Development\\Video 87\\Aman.txt"
+console.log(path.extname(myPath))
+console.log(path.dirname(myPath))
+console.log(path.basename(myPath))
+
+console.log(path.join("c:/","programs\\oneDrive\\AshfaAnas"))
