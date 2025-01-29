ec93dd4bd67132adcfe7caeefe07b590db5bfdd5
Mohammad Aman <114508170+MohammadAman5577@users.noreply.github.com>
2025-01-29 22:06:14 +0530

    Create mymodule.js

---
+++ Video 86/mymodule.js
@@ -0,0 +1,12 @@
+// export const a = 1;
+// export const b = 2;  //named export : matlab name is necessary for importing
+// export const c = 31;  comman js
+// export const d = 14;
+// export const e = 41;
+
+
+const obj ={             //ecma script
+    m:30,
+    n:69
+}
+export default obj;
