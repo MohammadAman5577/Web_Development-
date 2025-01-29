0b4d9f185fb7edef99ada871f041b784fd0b12d4
Mohammad Aman <114508170+MohammadAman5577@users.noreply.github.com>
2025-01-29 22:06:05 +0530

    Create main.js

---
+++ Video 86/main.js
@@ -0,0 +1,7 @@
+import aman from "./mymodule.js"
+console.log(aman)
+
+// const a = require("./mymodule2.js")
+// console.log(a)
+// yeah pe a ki jagah koi bhi variavle use kar sakte hai 
+// kyouki default export me same naam use karna important nahi hota
